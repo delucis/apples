efad8a703a2887bea3ae3c4bfaf028d030e9f12b
delucis <swithinbank@gmail.com>
2017-06-21 19:43:52 +0200

    Create function to display copyright information in site footer.

--- functions.php
+++ functions.php
@@ -171,6 +171,32 @@ function twentytwelve_entry_meta() {
 }
 
 
+// Function for dynamic copyright date in footer
+function site_copyright() {
+  global $wpdb;
+  $copyright_dates = $wpdb->get_results("
+  SELECT
+  YEAR(min(post_date_gmt)) AS firstdate,
+  YEAR(max(post_date_gmt)) AS lastdate
+  FROM
+  $wpdb->posts
+  WHERE
+  post_status = 'publish'
+  ");
+  $rightsholder = get_bloginfo('name');
+  $output = '';
+  if($copyright_dates) {
+    $copyright = "Copyright &copy; " . $copyright_dates[0]->firstdate;
+    if($copyright_dates[0]->firstdate != $copyright_dates[0]->lastdate) {
+      $copyright .= '-' . $copyright_dates[0]->lastdate;
+    }
+    $copyright .= ' ' . $rightsholder;
+    $output = $copyright;
+  }
+  return $output;
+}
+
+
 
 /** Add Custom logo to log-in page */
 function my_login_logo() { ?>
