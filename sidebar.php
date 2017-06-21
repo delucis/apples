861e6f5f69650f2bea6bf9162ea68e136d0c7769
delucis <swithinbank@gmail.com>
2017-06-21 19:45:37 +0200

    Remove defunct call to is_woocommerce() in sidebar.

--- sidebar.php
+++ sidebar.php
@@ -9,13 +9,7 @@
  * @since Twenty Twelve 1.0
  */
 ?>
-	<?php if ( is_woocommerce() ) : ?>
-		<?php if ( is_active_sidebar( 'sidebar-wc' ) ) : ?>
-			<div id="secondary" class="widget-area" role="complementary">
-				<?php dynamic_sidebar( 'sidebar-wc' ); ?>
-			</div><!-- #secondary -->
-		<?php endif; ?>
-	<?php elseif ( is_front_page() || is_archive() || is_single() && ! has_post_thumbnail() ) : ?>
+	<?php if ( is_front_page() || is_archive() || is_single() && ! has_post_thumbnail() ) : ?>
 		<?php if ( is_active_sidebar( 'sidebar-4' ) ) : ?>
 			<div id="secondary" class="widget-area" role="complementary">
 				<?php dynamic_sidebar( 'sidebar-4' ); ?>
